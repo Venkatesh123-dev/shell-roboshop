b8820e12d2ab454ff9fed19ca36912511d40ef9a
Venkatesh <tvenky359@gmail.com>
2025-09-27 11:39:04 +0530

    Changes done

--- user.sh
+++ user.sh
@@ -8,21 +8,18 @@ N="\e[0m"
 
 LOGS_FOLDER="/var/log/shell-roboshop"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
-LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
 SCRIPT_DIR=$PWD
-MONGODB_HOST="mongodb.venkatesh.fun"
-START_TIME=$(date +%s)
+MONGODB_HOST=mongodb.daws86s.fun
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
+
 mkdir -p $LOGS_FOLDER
 echo "Script started executed at: $(date)" | tee -a $LOG_FILE
 
-### Checking For Root Access #####
 if [ $USERID -ne 0 ]; then
     echo "ERROR:: Please run this script with root privelege"
     exit 1 # failure is other than 0
 fi
 
-
-#### Validate Function #####
 VALIDATE(){ # functions receive inputs through args just like shell script args
     if [ $1 -ne 0 ]; then
         echo -e "$2 ... $R FAILURE $N" | tee -a $LOG_FILE
@@ -32,55 +29,46 @@ VALIDATE(){ # functions receive inputs through args just like shell script args
     fi
 }
 
-### NodeJs ###
-dnf module disable nodejs -y &>>LOG_FILE
-VALIDATE $? "disable nodejs"
+##### NodeJS ####
+dnf module disable nodejs -y &>>$LOG_FILE
+VALIDATE $? "Disabling NodeJS"
+dnf module enable nodejs:20 -y  &>>$LOG_FILE
+VALIDATE $? "Enabling NodeJS 20"
+dnf install nodejs -y &>>$LOG_FILE
+VALIDATE $? "Installing NodeJS"
 
-dnf module enable nodejs:20 -y &>>LOG_FILE
-VALIDATE $? "enableing  nodejs:20"
-
-dnf install nodejs -y &>>LOG_FILE
-VALIDATE $? "installing  nodejs"
-
-
-id roboshop &>>LOG_FILE
+id roboshop &>>$LOG_FILE
 if [ $? -ne 0 ]; then
     useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
-    VALIDATE $? "roboshop system user"
+    VALIDATE $? "Creating system user"
 else
-    echo -e " roboshop system user already exist ... $Y SKIPPING $N" 
+    echo -e "User already exist ... $Y SKIPPING $N"
 fi
 
-mkdir -p /app 
-VALIDATE $? "Creaing app directory"
+mkdir -p /app
+VALIDATE $? "Creating app directory"
 
-curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user-v3.zip &>>$LOG_FILE
-VALIDATE $? "Downloading  user application"
+curl -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user-v3.zip &>>$LOG_FILE
+VALIDATE $? "Downloading user application"
 
 cd /app 
-VALIDATE $? "Changing app directory"
+VALIDATE $? "Changing to app directory"
 
-rm -rf /app &>>$LOG_FILE
-VALIDATE $? "Removing existing app code"
+rm -rf /app/*
+VALIDATE $? "Removing existing code"
 
 unzip /tmp/user.zip &>>$LOG_FILE
 VALIDATE $? "unzip user"
 
-
 npm install &>>$LOG_FILE
-VALIDATE $? "Installing depecncies"
+VALIDATE $? "Install dependencies"
 
-cp $SCRIPT_DIR/user.service /etc/systemd/system/user.service &>>$LOG_FILE
-VALIDATE $? "Copy user service"
+cp $SCRIPT_DIR/user.service /etc/systemd/system/user.service
+VALIDATE $? "Copy systemctl service"
 
 systemctl daemon-reload
-systemctl enable user &>>LOG_FILE
-VALIDATE $? "enable user"
+systemctl enable user &>>$LOG_FILE
+VALIDATE $? "Enable user"
 
-systemctl restart user &>>LOG_FILE
+systemctl restart user
 VALIDATE $? "Restarted user"
-
-
-END_TIME=$(date +%s)
-TOTAL_TIME=$(( $END_TIME - $START_TIME ))
-echo -e "Script executed in: $Y $TOTAL_TIME Seconds $N"
